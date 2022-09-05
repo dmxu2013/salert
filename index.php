74b805215a1d8b45321a16227c66aa0014fad61f
Dongmin Xu <dmxu2001@yahoo.com>
2022-09-05 10:43:13 -0700

    use html tag pre

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
   <title>Alert Test</title>
  </head>
  <body>
-  <p> ADSK GE 200.89 </p>
-  <p> IBM  GE 129.05 </p>
-  <p> MSFT GE 258.67 </p>
-  <p> NFLX GE 227.80 </p>
-  <p> QQQ  GE 298.52 </p>
- <p> SPY  GE 395.82 </p>
+  <pre>
+    ADSK GE 200.89
+    IBM  GE 129.05
+    MSFT GE 258.67
+    NFLX GE 227.80
+    QQQ  GE 298.52
+    SPY  GE 395.82
+  </pre>
  </body>
 </html>
