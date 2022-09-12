d927ac5ea3b0c772155114cffe576b8d416f8453
Dongmin Xu <dmxu2001@yahoo.com>
2022-09-11 20:08:26 -0700

    20220911

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
     AAL  GE 15.0
-    IBM  GE 127.77
+    DIS  GE 116.0
+    HKD  GE 100.0
     JNUG GE 30.0 
-    NXE  GE 4.65
-    QQQ  GE 297.52
-    UCO  GE 33.35
-    URA  GE 23.83
-    YINN GE 57.45
+    NUGT GE 35.0
+    NXE  GE 4.85
+    UCO  GE 33.5
+    UPST GE 35.5
+    URA  GE 24.0
+    YINN GE 59.45
